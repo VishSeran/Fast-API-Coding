c6db5039a1641d933b238c19e4312e8cdf3233e5
VishSeran <vishwa.sovis@gmail.com>
2026-03-12 21:05:47 +0530

    patch method updated

--- main.py
+++ main.py
@@ -97,17 +97,19 @@ def shipment_update(id: int, content: str, weight:float, status: str
     return shipments[id]
 
 @app.patch("/shipment")
-def patch_shipment(id:int, content:str | None = None, 
-                   weight:str | None = None, 
-                   status:str | None = None):
+def patch_shipment(id:int, body: dict[str, Any]) -> dict[str, Any]:
     
-    shipment = shipments[id]
-    if content:
-        shipment["content"] = content
-    if weight:
-        shipment["weight"] = weight
-    if status:
-        shipment["status"] = status
+    #shipment = shipments[id]
+    # if content:
+    #     shipment["content"] = content
+    # if weight:
+    #     shipment["weight"] = weight
+    # if status:
+    #     shipment["status"] = status
+    
+    shipments[id].update(body)
+    return shipments[id]
+    
     
 
 
