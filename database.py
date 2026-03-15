a65f7bb7bb51366d2cd567304b5435a7bdc8b59b
VishSeran <vishwa.sovis@gmail.com>
2026-03-15 13:07:04 +0530

    database crud updated

--- database.py
+++ database.py
@@ -8,19 +8,51 @@ cursor = connection.cursor()# make the cursor object
 # 1. Create table
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS shipment (
-        id INTEGER,
+        id INTEGER PRIMARY KEY,
         content TEXT,
         weight REAL,
         status TEXT
     )
                """)
 
+# cursor.execute("DROP TABLE shipment")
+# connection.commit()
+
 # 2. Add shipment data
+# cursor.execute("""
+#         INSERT INTO shipment 
+#         VALUES (3, 'plam tree', 12.5, 'in_transit')           
+#     """)
+# connection.commit()
+
+# 3. Fetch data from database
+# cursor.execute("""
+#          SELECT * FROM shipment
+#          WHERE status = 'placed'      
+#                """)
+# result = cursor.fetchmany(2)
+# print(result)
+
+# 4. update a shipment
+
+id = 0
+status = 'placed'
 cursor.execute("""
-        INSERT INTO shipment 
-        VALUES (12751, 'Olive tree', 19.5, 'in_transit')           
-    """)
+    UPDATE shipment SET status = :status
+    WHERE id > :id
+               """,
+               {
+                   "status" : status,
+                   "id": id
+               })
 connection.commit()
 
+#5. Delete a shipment by id
+
+# cursor.execute("""
+#          DELETE FROM shipment
+#          WHERE id =  12752
+#                """)
+# connection.commit()
 # Close Connection when done
 connection.close()
