e748ebf6682d0570af4335b9ed01239c172f8d3a
VishSeran <vishwa.sovis@gmail.com>
2026-03-15 17:16:46 +0530

    API endpoints updated

--- database.py
+++ database.py
@@ -1,22 +1,22 @@
 import sqlite3
 from typing import Any
 
-from schemas import ShipmentCreate, ShipmentUpdate
+from .schemas import ShipmentCreate, ShipmentUpdate
+
 
 class Database:
-    
     def __init__(self):
         # make the conncetion
-        self.connection = sqlite3.connect("ShipmentDatabase.db")
-        #Cursor object for query executions
+        self.connection = sqlite3.connect("sqlite.db", check_same_thread=False)
+        # Cursor object for query executions
         self.cursor = self.connection.cursor()
         self.create_table()
-        
+
     # create table
     def create_table(self):
         # 1. Create table
         self.cursor.execute("""
-            CREATE TABLE IF NOT EXIST shipment (
+            CREATE TABLE IF NOT EXISTS shipment (
                 id INTEGER PRIMARY KEY,
                 content TEXT,
                 weight REAL,
@@ -25,66 +25,69 @@ class Database:
                 """)
 
     # 2. Add shipment data
-    def create(self, shipment:ShipmentCreate) -> int:
+    def create(self, shipment: ShipmentCreate) -> int:
         self.cursor.execute("""
                 SELECT MAX(id) FROM shipment            
                             """)
-        result = (self.cursor.fetchone()) + 1
+        result = (self.cursor.fetchone())
         new_id = result[0] + 1
-        self.cursor.execute("""
+        self.cursor.execute(
+            """
             INSERT INTO shipment 
             VALUES (:id, :content, :weight, :status)
                             """,
-            {
-                "id": new_id,
-                **shipment.model_dump(),
-                "status": "placed"
-            })
+            {"id": new_id, **shipment.model_dump(), "status": "placed"},
+        )
         self.connection.commit()
         return new_id
 
     # get shipment
-    def get(self, id:int)-> dict[str, Any] | None:
-        
-        self.cursor.execute("""
+    def get(self, id: int) -> dict[str, Any] | None:
+
+        self.cursor.execute(
+            """
             SELECT * FROM shipment WHERE id = ?                            
-                            """, (id,))
+                            """,
+            (id,),
+        )
         result = self.cursor.fetchone()
-        
+
         if result is None:
             return None
-             
+
         return {
-            "id" : result[0],
+            "id": result[0],
             "content": result[1],
             "weight": result[2],
-            "status": result[3]
+            "status": result[3],
         }
-        
+
     # update a shipment
-    def update(self, id:int, shipment: ShipmentUpdate)-> dict[str, Any] | None:
-        self.cursor.execute("""
-            UPDATE shipment SET status = :status
-            WHERE id = :id                
-            """,{
-                "id": id,
-                **shipment.model_dump()
-            })
+    def update(self, id: int, shipment: ShipmentUpdate) -> dict[str, Any] | None:
+        self.cursor.execute(
+            """
+            UPDATE shipment 
+            SET content = :content, 
+                weight = :weight, 
+                status = :status
+            WHERE id = :id               
+            """,
+            {"id": id, **shipment.model_dump()},
+        )
         self.connection.commit()
         return self.get(id)
 
     # delete a shipment
-    def delete(self, id:int):
-        self.cursor.execute("""
+    def delete(self, id: int):
+        self.cursor.execute(
+            """
             DELETE FROM shipment 
             WHERE id = ?                
-            """,(id,))
+            """,
+            (id,),
+        )
         self.connection.commit()
-    
+
     # close the database connection
     def close(self):
         self.connection.close()
-
-
-
-
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, status, HTTPException
 from scalar_fastapi import get_scalar_api_reference
 from typing import Any
 from .schemas import  ShipmentCreate, ShipmentRead, ShipmentUpdate
-from .database_json import shipments,save
 from .database import Database
 
 
@@ -20,7 +19,7 @@ def get_shipment_by_id(id: int):
             status_code=status.HTTP_404_NOT_FOUND, detail="Id does not exists"
         )
     
-    return shipments
+    return shipment
     
 
 
@@ -61,56 +60,8 @@ def delete_shipment(id:int) -> dict[str,str]:
     db.delete(id)
     return {"Details": "shipment with id {} is deleted".format(id)}
     
-    
-
 
 @app.get("/scalar", include_in_schema=False)
 def get_scalar_doc():
     return get_scalar_api_reference(openapi_url=app.openapi_url, title="Scalar FastAPI")
 
-
-digits: list[int] = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-]  # List type variable which contains int type values
-
-number: int | float = 12.2
-tuple_1: tuple[int, int, int, int] = (1, 2, 3, 4)
-
-city = City("Sri lanka", "ja-ela")
-city_temp: tuple[City, float] = (city, 20.5)
-
-shipment: dict[str, str | int | float] = {
-    "id": 1234,
-    "weight": 1.43,
-    "content": "wooden table",
-    "status": "in transit",
-}
-
-shipmentNew: dict[str, Any] = {
-    "id": 1234,
-    "weight": 1.43,
-    "content": "wooden table",
-    "status": "in transit",
-}
-
-
-def root(num: int | float, exp: float | None) -> float:
-
-    if exp is None:
-        exp = 0.5
-    else:
-        exp = exp
-
-    return pow(num, exp)
-
-
-root_25 = root(25, 10)
-root_25
