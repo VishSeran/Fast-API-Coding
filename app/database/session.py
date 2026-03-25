3aef26b66d17c3b4708abcd19df8d2a12178765b
VishSeran <vishwa.sovis@gmail.com>
2026-03-25 12:29:26 +0530

    engine update and create db update with async"

--- app/database/session.py
+++ app/database/session.py
@@ -1,22 +1,36 @@
 
 from typing import Annotated
 from fastapi import Depends
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_engine, create_async_engine
 from sqlmodel import SQLModel, Session
 
-engine = create_engine (
-    url="sqlite:///new_sqlite_2.db",
-    echo=True,
-    connect_args={"check_same_thread": False}
+from app.config import DatabaseSettings
+
+# engine = create_engine (
+#     url="sqlite:///new_sqlite_2.db",
+#     echo=True,
+#     connect_args={"check_same_thread": False}
+# )
+
+# from .model import Shipment
+# def create_db():
+#     SQLModel.metadata.create_all(bind=engine)
+
+
+# def session_bind():
+#     with Session(bind=engine) as session:
+#         yield session
+
+# SessionDep = Annotated[Session, Depends(session_bind)]
+settings = DatabaseSettings()
+
+engine = create_async_engine(
+    url= settings.POSTGRES_URL,
+    echo = True
 )
 
-from .model import Shipment
-def create_db():
-    SQLModel.metadata.create_all(bind=engine)
-
-
-def session_bind():
-    with Session(bind=engine) as session:
-        yield session
-
-SessionDep = Annotated[Session, Depends(session_bind)]
+async def create_db():
+    # context manager
+    async with engine.begin() as conncetion:
+        await conncetion.run_sync(SQLModel.metadata.create_all())
+        
